1e02195226bb1854608410c651987c6c98943bc6
nesty <neskul01@iba.dk>
2025-05-19 15:22:57 +0200

    Update site URL in astro.config.mjs to reflect the correct deployment path

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://synku20777.github.io',
+  site: 'https://synku20777.github.io/hovedopgave-landing',
   base: 'hovedopgave-landing',
   // output: 'server',
   // adapter: node(),
